b4fdcab7fc7c699cb8a805e4f7fba8f874a6d60e
Thvarzil <louisblebohec@gmail.com>
2021-11-23 17:29:52 -0700

    GUI Progress

---
+++ src/blackjack/BlackjackGUI.java
@@ -0,0 +1,209 @@
+package blackjack;
+
+import java.awt.BorderLayout;
+import java.awt.EventQueue;
+
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.border.EmptyBorder;
+import javax.swing.JLayeredPane;
+import java.awt.GridLayout;
+import javax.swing.JButton;
+import java.awt.Color;
+import java.awt.Font;
+import javax.swing.JTextPane;
+import javax.swing.JLabel;
+import java.awt.FlowLayout;
+import javax.swing.JSplitPane;
+import javax.swing.SwingConstants;
+
+@SuppressWarnings("serial")
+public class BlackjackGUI extends JFrame {
+
+	private JPanel contentPane;
+
+	/**
+	 * Launch the application.
+	 */
+	public static void main(String[] args) {
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					BlackjackGUI frame = new BlackjackGUI();
+					frame.setVisible(true);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+	}
+
+	/**
+	 * Create the frame.
+	 */
+	public BlackjackGUI() {
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		setBounds(100, 100, 677, 484);
+		contentPane = new JPanel();
+		contentPane.setBorder(new EmptyBorder(0, 0, 0, 0));
+		contentPane.setLayout(new BorderLayout(0, 0));
+		setContentPane(contentPane);
+		
+		JLayeredPane layeredPane = newLayeredPane();
+		contentPane.add(layeredPane, BorderLayout.CENTER);
+	
+		
+	}
+
+	private JLayeredPane newLayeredPane() {
+		JLayeredPane layeredPane = new JLayeredPane();
+		layeredPane.setOpaque(true);
+		layeredPane.setBackground(new Color(0, 102, 51));
+		
+		JPanel PlayersCards = newPlayersCards();
+		layeredPane.add(PlayersCards);
+		
+		JPanel DealersCards = newDealersCards();
+		layeredPane.add(DealersCards);
+
+		JPanel BettingPanel = newBettingPanel();
+		layeredPane.add(BettingPanel);
+	
+		JPanel panelEndOfGame = newPanelEndOfGame();
+		
+		layeredPane.add(panelEndOfGame);
+		
+		JPanel HitStayPanel = newHitStayPanel();
+		layeredPane.add(HitStayPanel);
+		{
+			JLabel lblNewLabel = new JLabel("You Win!!");
+			lblNewLabel.setForeground(new Color(255, 255, 255));
+			lblNewLabel.setFont(new Font("Monospaced", Font.BOLD, 46));
+			lblNewLabel.setHorizontalAlignment(SwingConstants.CENTER);
+			lblNewLabel.setBounds(10, 26, 285, 38);
+			layeredPane.add(lblNewLabel);
+		}
+		
+		return layeredPane;
+	}
+
+	private JPanel newHitStayPanel() {
+		JPanel HitStayPanel = new JPanel();
+		HitStayPanel.setBounds(47, 202, 217, 38);
+		HitStayPanel.setLayout(new GridLayout(0, 2, 0, 0));
+		
+		JButton btnHit = newBtnHit();
+		HitStayPanel.add(btnHit);
+	
+		JButton btnStay = newBtnStay();
+		HitStayPanel.add(btnStay);
+		
+		return HitStayPanel;
+	}
+
+	private JButton newBtnStay() {
+		JButton btnStay = new JButton("Stay");
+		btnStay.setFont(new Font("Monospaced", Font.PLAIN, 11));
+		return btnStay;
+	}
+
+	private JButton newBtnHit() {
+		JButton btnHit = new JButton("Hit");
+		btnHit.setFont(new Font("Monospaced", Font.PLAIN, 11));
+		return btnHit;
+	}
+
+	private JPanel newPanelEndOfGame() {
+		JPanel panelEndOfGame = new JPanel();
+		panelEndOfGame.setBounds(47, 75, 217, 38);
+		panelEndOfGame.setLayout(new GridLayout(1, 0, 0, 0));
+		
+		JButton btnPlayAgain = newbtnPlayAgain();
+		panelEndOfGame.add(btnPlayAgain);
+	
+		JButton btnClose = newbtnClose();
+		panelEndOfGame.add(btnClose);
+		
+		return panelEndOfGame;
+	}
+
+	private JButton newbtnClose() {
+		JButton btnClose = new JButton("End");
+		btnClose.setFont(new Font("Monospaced", Font.PLAIN, 11));
+		btnClose.addActionListener(e -> System.exit(0));
+
+		return btnClose;
+	}
+
+	private JButton newbtnPlayAgain() {
+		JButton btnPlayAgain = new JButton("Play Again");
+		btnPlayAgain.setFont(new Font("Monospaced", Font.PLAIN, 11));
+		return btnPlayAgain;
+	}
+
+	private JPanel newBettingPanel() {
+		JPanel BettingPanel = new JPanel();
+		BettingPanel.setBorder(new EmptyBorder(0, 0, 0, 0));
+		BettingPanel.setBounds(90, 290, 134, 113);
+		BettingPanel.setLayout(new GridLayout(0, 1, 0, 0));
+		
+		JPanel BidPanel = newBidPanel();
+		BettingPanel.add(BidPanel);
+		
+		JButton btnIncreaseBet = newbtnIncreaseBet();
+		BettingPanel.add(btnIncreaseBet);
+
+		JButton btnDecreaseBet = newbtnDecreaseBet();
+		BettingPanel.add(btnDecreaseBet);
+		
+		JButton btnSubmit = newbtnSubmit();
+		BettingPanel.add(btnSubmit);
+		
+		return BettingPanel;
+	}
+
+	private JPanel newBidPanel() {
+		JPanel BidPanel = new JPanel();
+		BidPanel.setBorder(new EmptyBorder(0, 10, 0, 0));
+		BidPanel.setLayout(new BorderLayout(0, 0));
+		
+		JLabel CurrentBet = newCurrentBet();
+		BidPanel.add(CurrentBet);
+		
+		return BidPanel;
+	}
+
+	private JLabel newCurrentBet() {
+		JLabel CurrentBet = new JLabel("Current Bet: 100");
+		CurrentBet.setFont(new Font("Monospaced", Font.PLAIN, 11));
+		return CurrentBet;
+	}
+
+	private JButton newbtnSubmit() {
+		JButton btnSubmit = new JButton("Submit");
+		btnSubmit.setFont(new Font("Monospaced", Font.PLAIN, 11));
+		return btnSubmit;
+	}
+
+	private JButton newbtnDecreaseBet() {
+		JButton btnDecreaseBet = new JButton("\u25BC");
+		return btnDecreaseBet;
+	}
+
+	private JButton newbtnIncreaseBet() {
+		JButton btnIncreaseBet = new JButton("\u25B2");
+		return btnIncreaseBet;
+	}
+
+	private JPanel newPlayersCards() {
+		JPanel PlayersCards = new PlayersCards();
+		PlayersCards.setBounds(320, 256, 335, 182);
+		return PlayersCards;
+	}
+	
+	private JPanel newDealersCards() {
+		JPanel DealersCards = new DealersCards();
+		DealersCards.setBounds(320, 11, 335, 182);
+		return DealersCards;
+	}
+}
---
+++ src/blackjack/PlayersCards.java
@@ -0,0 +1,111 @@
+package blackjack;
+
+import javax.swing.JPanel;
+import javax.swing.JLayeredPane;
+import javax.swing.JLabel;
+import javax.swing.ImageIcon;
+import java.awt.BorderLayout;
+import java.awt.Color;
+
+@SuppressWarnings("serial")
+public class PlayersCards extends JPanel {
+	//Cards start on the left at 8 and decrease as they get moved to the right on the panel
+	public String card1Path = "";
+	public String card2Path = "";
+	public String card3Path = "";
+	public String card4Path = "";
+	public String card5Path = "/img/10_of_clubs.png";
+	public String card6Path = "/img/10_of_clubs.png";
+	public String card7Path = "/img/10_of_clubs.png";
+	public String card8Path = "/img/10_of_clubs.png";
+
+	public PlayersCards() {
+		setBackground(new Color(0, 100, 0));
+		setLayout(new BorderLayout(0, 0));
+		
+		JLayeredPane layeredPane = new JLayeredPane();
+		layeredPane.setOpaque(true);
+		layeredPane.setBackground(new Color(0, 100, 0));
+		add(layeredPane);
+		
+		JLabel card1 = newCard1();
+		layeredPane.add(card1);
+		
+		JLabel card2 = newCard2();
+		layeredPane.add(card2);
+		
+		JLabel card3 = newCard3();
+		layeredPane.add(card3);
+		
+		JLabel card4 = newCard4();
+		layeredPane.add(card4);
+		
+		JLabel card5 = newCard5();
+		layeredPane.add(card5);
+
+		JLabel card6 = newCard6();
+		layeredPane.add(card6);
+		
+		JLabel card7 = newCard7();
+		layeredPane.add(card7);
+		
+		JLabel card8 = newCard8();
+		layeredPane.add(card8);
+	}
+
+	private JLabel newCard8() {
+		JLabel card8 = new JLabel("");
+		card8.setIcon(new ImageIcon(PlayersCards.class.getResource(card8Path)));
+		card8.setBounds(0, 0, 125, 182);
+		return card8;
+	}
+
+	private JLabel newCard7() {
+		JLabel card7 = new JLabel("");
+		card7.setIcon(new ImageIcon(PlayersCards.class.getResource(card7Path)));
+		card7.setBounds(30, 0, 125, 182);
+		return card7;
+	}
+
+	private JLabel newCard6() {
+		JLabel card6 = new JLabel("");
+		card6.setIcon(new ImageIcon(PlayersCards.class.getResource(card6Path)));
+		card6.setBounds(60, 0, 125, 182);
+		return card6;
+	}
+
+	private JLabel newCard5() {
+		JLabel card5 = new JLabel("");
+		card5.setIcon(new ImageIcon(PlayersCards.class.getResource(card5Path)));
+		card5.setBounds(90, 0, 125, 182);
+		return card5;
+	}
+
+	private JLabel newCard4() {
+		JLabel card4 = new JLabel("");
+		card4.setIcon(new ImageIcon(PlayersCards.class.getResource(card4Path)));
+		card4.setBounds(120, 0, 125, 182);
+		return card4;
+	}
+
+	private JLabel newCard3() {
+		JLabel card3 = new JLabel("");
+		card3.setIcon(new ImageIcon(PlayersCards.class.getResource(card3Path)));
+		card3.setBounds(150, 0, 125, 182);
+		return card3;
+	}
+	
+	private JLabel newCard2() {
+		JLabel card2 = new JLabel("");
+		card2.setIcon(new ImageIcon(PlayersCards.class.getResource(card2Path)));
+		card2.setBounds(180, 0, 125, 182);
+		return card2;
+	}
+	
+	private JLabel newCard1() {
+		JLabel card1 = new JLabel("");
+		card1.setIcon(new ImageIcon(PlayersCards.class.getResource(card1Path)));
+		card1.setBounds(210, 0, 125, 182);
+		return card1;
+	}
+}
